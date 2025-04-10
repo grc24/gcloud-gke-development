bdbfe9bd246a992d034f8e1c16aab59a4012d67a
SEIDOU <david.seidou@iviplay.com>
2025-04-10 19:26:22 +0100

    add size

--- main.tf
+++ main.tf
@@ -57,48 +57,92 @@ data "google_container_engine_versions" "gke_version" {
   version_prefix = var.gke_version_prefix
 }
 
+# resource "google_container_cluster" "primary" {
+#   name     = "${var.project_id}-gke"
+#   location = var.gcp_region
+
+#   # We can't create a cluster with no node pool defined, but we want to only use
+#   # separately managed node pools. So we create the smallest possible default
+#   # node pool and immediately delete it.
+#   remove_default_node_pool = true
+#   initial_node_count       = 1
+
+#   network    = google_compute_network.vpc.name
+#   subnetwork = google_compute_subnetwork.subnet.name
+# }
+
+# # Separately Managed Node Pool
+# resource "google_container_node_pool" "primary_nodes" {
+#   name     = google_container_cluster.primary.name
+#   location = var.gcp_region
+#   cluster  = google_container_cluster.primary.name
+
+#   version    = data.google_container_engine_versions.gke_version.release_channel_default_version["STABLE"]
+#   node_count = var.gke_num_nodes
+
+#   node_config {
+#     oauth_scopes = [
+#       "https://www.googleapis.com/auth/logging.write",
+#       "https://www.googleapis.com/auth/monitoring",
+#     ]
+
+#     labels = {
+#       env = var.project_id
+#     }
+
+#     # preemptible  = true
+#     machine_type = "n1-standard-1"
+#     tags         = ["gke-node", "${var.project_id}-gke"]
+#     metadata = {
+#       disable-legacy-endpoints = "true"
+#     }
+#   }
+# }
 resource "google_container_cluster" "primary" {
   name     = "${var.project_id}-gke"
   location = var.gcp_region
 
-  # We can't create a cluster with no node pool defined, but we want to only use
-  # separately managed node pools. So we create the smallest possible default
-  # node pool and immediately delete it.
-  remove_default_node_pool = true
-  initial_node_count       = 1
+  initial_node_count = 1
 
   network    = google_compute_network.vpc.name
   subnetwork = google_compute_subnetwork.subnet.name
-}
-
-# Separately Managed Node Pool
-resource "google_container_node_pool" "primary_nodes" {
-  name     = google_container_cluster.primary.name
-  location = var.gcp_region
-  cluster  = google_container_cluster.primary.name
-
-  version    = data.google_container_engine_versions.gke_version.release_channel_default_version["STABLE"]
-  node_count = var.gke_num_nodes
 
   node_config {
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
-
-    labels = {
-      env = var.project_id
-    }
     machine_type = "e2-medium"
-
+    disk_size_gb = 10 # 👈 very small disk to avoid SSD quota issue
     disk_type    = "pd-ssd"
-    disk_size_gb = 50
-
-    # preemptible  = true
-    #machine_type = "n1-standard-1"
-    tags = ["gke-node", "${var.project_id}-gke"]
-    metadata = {
-      disable-legacy-endpoints = "true"
-    }
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
   }
 }
+
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "primary-node-pool"
+  location   = var.gcp_region
+  cluster    = google_container_cluster.primary.name
+  node_count = 3
+
+  node_config {
+    machine_type = "e2-medium"
+    disk_type    = "pd-ssd"
+    disk_size_gb = 50
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+  }
+
+  management {
+    auto_upgrade = true
+    auto_repair  = true
+  }
+}
+
+resource "null_resource" "delete_default_node_pool" {
+  provisioner "local-exec" {
+    command = "gcloud container node-pools delete default-pool --cluster=${google_container_cluster.primary.name} --region=${var.gcp_region} --quiet"
+  }
+
+  depends_on = [google_container_cluster.primary]
+}
+
