b5ce16452339f82907c9b513c8e584112a0de2aa
SEIDOU <david.seidou@iviplay.com>
2025-04-10 14:40:43 +0100

    before create github-repository

---
+++ main.tf
@@ -0,0 +1,17 @@
+resource "github_repository" "main" {
+  name =  var.repository_name
+  description = "Terraform create repository for Google GKE"
+  visibility = "public"
+  auto_init = true
+  gitignore_template = "Terraform"
+}
+
+resource "github_branch" "main" {
+  repository = github_repository.main.name
+  branch = "main"
+}
+
+resource "github_branch_default" "main" {
+  repository = github_repository.main.name
+  branch = github_branch.main.branch
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "repository_name" {
+  default = "gcloud-gke-development"
+  description = "Name of repository for gke"
+  type = string
+}
