825e6dbf6f672132dc99ba660b32c593e208a057
SEIDOU <david.seidou@iviplay.com>
2025-04-11 16:57:50 +0100

    CHange type of server and size

--- main.tf
+++ main.tf
@@ -108,41 +108,41 @@ resource "google_container_cluster" "primary" {
   subnetwork = google_compute_subnetwork.subnet.name
 
   node_config {
-    machine_type = "e2-medium"
-    disk_size_gb = 10 # 👈 very small disk to avoid SSD quota issue
+    machine_type = "e2-standard-2"
+    disk_size_gb = 50
     disk_type    = "pd-ssd"
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform",
     ]
   }
 }
-resource "google_container_node_pool" "primary_nodes" {
-  name       = "primary-node-pool"
-  location   = var.gcp_region
-  cluster    = google_container_cluster.primary.name
-  node_count = 2 # 👈 reduce number of nodes
+# resource "google_container_node_pool" "primary_nodes" {
+#   name       = "primary-node-pool"
+#   location   = var.gcp_region
+#   cluster    = google_container_cluster.primary.name
+#   node_count = 2 
 
-  node_config {
-    machine_type = "e2-small" # 👈 smaller CPU
-    disk_type    = "pd-ssd"
-    disk_size_gb = 50 # 👈 reduce disk size
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform",
-    ]
-  }
+#   node_config {
+#     machine_type = "e2-small" 
+#     disk_type    = "pd-ssd"
+#     disk_size_gb = 50 
+#     oauth_scopes = [
+#       "https://www.googleapis.com/auth/cloud-platform",
+#     ]
+#   }
 
-  management {
-    auto_upgrade = true
-    auto_repair  = true
-  }
-}
+#   management {
+#     auto_upgrade = true
+#     auto_repair  = true
+#   }
+# }
 
 
-resource "null_resource" "delete_default_node_pool" {
-  provisioner "local-exec" {
-    command = "gcloud container node-pools delete default-pool --cluster=${google_container_cluster.primary.name} --region=${var.gcp_region} --quiet"
-  }
+# resource "null_resource" "delete_default_node_pool" {
+#   provisioner "local-exec" {
+#     command = "gcloud container node-pools delete default-pool --cluster=${google_container_cluster.primary.name} --region=${var.gcp_region} --quiet"
+#   }
 
-  depends_on = [google_container_cluster.primary]
-}
+#   depends_on = [google_container_cluster.primary]
+# }
 
