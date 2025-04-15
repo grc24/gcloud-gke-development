27d5e16a5d0108affc4e2e0984683cc7da8f48a6
SEIDOU <david.seidou@iviplay.com>
2025-04-15 17:11:01 +0100

    add reload vcs settings

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
 output "http_clone_url" {
   value = github_repository.main.http_clone_url
-  description = "URL to use when adding remote to local git repo"
+  description = "URL to use when adding remote to local git repository"
 }
 
 output "tfe_worspace_id" {
