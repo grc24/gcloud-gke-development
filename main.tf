7a0c16bdfcc290453edd0f1d7463b998428b8172
SEIDOU <david.seidou@iviplay.com>
2025-04-15 18:36:27 +0100

    Set token in github secret 6

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
-resource "github_repository" "main" {
-  name               = var.repository_name
-  description        = "Terraform create repository for Google GKE"
-  visibility         = "public"
-  auto_init          = true
-  gitignore_template = "Terraform"
-}
+# resource "github_repository" "main" {
+#   name               = var.repository_name
+#   description        = "Terraform create repository for Google GKE"
+#   visibility         = "public"
+#   auto_init          = true
+#   gitignore_template = "Terraform"
+# }
 
-resource "github_branch" "main" {
-  repository = github_repository.main.name
-  branch     = "main"
-}
+# resource "github_branch" "main" {
+#   repository = github_repository.main.name
+#   branch     = "main"
+# }
 
-resource "github_branch_default" "main" {
-  repository = github_repository.main.name
-  branch     = github_branch.main.branch
-}
+# resource "github_branch_default" "main" {
+#   repository = github_repository.main.name
+#   branch     = github_branch.main.branch
+# }
 
 ### Terraform Ccloud configuration
 provider "tfe" {
--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
-output "http_clone_url" {
-  value       = github_repository.main.http_clone_url
-  description = "URL to use when adding remote to local git repository"
-}
+# output "http_clone_url" {
+#   value       = github_repository.main.http_clone_url
+#   description = "URL to use when adding remote to local git repository"
+# }
 
 output "tfe_worspace_id" {
   value       = tfe_oauth_client.github.id
